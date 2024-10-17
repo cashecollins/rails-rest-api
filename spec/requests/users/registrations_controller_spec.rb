12fd5d9bcd04ddffe6e7d35904f9d28e695f6338
cashecollins <cashecollins@gmail.com>
2024-10-17 10:11:06 -0600

    remove impact related naming conventions

--- spec/requests/users/registrations_controller_spec.rb
+++ spec/requests/users/registrations_controller_spec.rb
@@ -6,7 +6,7 @@ describe Users::RegistrationsController, type: :request  do
   describe 'Create a' do
     it 'user' do
       # Act
-      post "/api/v1/user", params: { user: { email: 'test@impactsuite.com', name: 'Test User', password: 'Password1!' } }
+      post "/api/v1/user", params: { user: { email: 'test@example.com', name: 'Test User', password: 'Password1!' } }
 
       # Assert
       expect(response).to have_http_status(:created)
